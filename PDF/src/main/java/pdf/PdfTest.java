e202f5cd6b69ab1173284a1ca5c9f497e1b81e21
cuiyunfei <sy714898922@sina.com>
2020-07-22 10:26:52 +0800

    commit1

---
+++ PDF/src/main/java/pdf/PdfTest.java
@@ -0,0 +1,67 @@
+package pdf;
+
+public class  PdfTest {
+    private Long id;
+    private String appNumber;
+    private Long idNo1;
+    private Long idNo2;
+    private String fill_1;
+    private String phoneForTrade2;
+    private String checkBox11;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getAppNumber() {
+        return appNumber;
+    }
+
+    public void setAppNumber(String appNumber) {
+        this.appNumber = appNumber;
+    }
+
+    public Long getIdNo1() {
+        return idNo1;
+    }
+
+    public void setIdNo1(Long idNo1) {
+        this.idNo1 = idNo1;
+    }
+
+    public Long getIdNo2() {
+        return idNo2;
+    }
+
+    public void setIdNo2(Long idNo2) {
+        this.idNo2 = idNo2;
+    }
+
+    public String getFill_1() {
+        return fill_1;
+    }
+
+    public void setFill_1(String fill_1) {
+        this.fill_1 = fill_1;
+    }
+
+    public String getPhoneForTrade2() {
+        return phoneForTrade2;
+    }
+
+    public void setPhoneForTrade2(String phoneForTrade2) {
+        this.phoneForTrade2 = phoneForTrade2;
+    }
+
+    public String getCheckBox11() {
+        return checkBox11;
+    }
+
+    public void setCheckBox11(String checkBox11) {
+        this.checkBox11 = checkBox11;
+    }
+}
